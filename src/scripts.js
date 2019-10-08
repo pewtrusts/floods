5456f0f88da8ff1263e4a71a27df0e351035c5ed
John Osterman <josterman@pewtrusts.org>
2019-10-08 11:09:56 -0400

    X-browser flipcard fixes

--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,20 @@
     var buttons = document.querySelectorAll('.js-mm-button');
     var items = document.querySelectorAll('.js-mm-section');
     function itemMouseHandler(){
+        var _this = this;
         this.classList.remove('is-selected');
         this.removeEventListener('mouseleave', itemMouseHandler);
+        setTimeout(function(){
+            _this.classList.remove('back-face-visible');
+        }, 200);
     }
     function itemClickHandler(){
+        var _this = this;
         this.classList.toggle('is-selected');
         this.addEventListener('mouseleave', itemMouseHandler)
+        setTimeout(function(){
+            _this.classList.toggle('back-face-visible');
+        }, 200);
     }
     // using `for` loop to avoid need for NodeList.forEach polyfill
     for ( var i = 0; i < buttons.length; i++ ){
