c15366cb8052be608adaa347c8c50367aba05fe9
John Osterman <josterman@pewtrusts.org>
2019-11-01 10:33:12 -0400

    Config delets overview from dist html

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,7 @@ const prerender = new PrerenderSPAPlugin({
              renderedRoute.html = renderedRoute.html.replace(/class="emitted-css" href="(.*?)"/g,'class="emitted-css" href="' + publicPath + '$1' + '"');
              renderedRoute.html = renderedRoute.html.replace(/class="emitted-bundle" src="(.*?)"/g,'class="emitted-bundle" src="' + publicPath + '$1' + '"');
              renderedRoute.html = renderedRoute.html.replace(/<\/?head>/g,'').replace(/<\/?html.*?>|<\/?body.*?>/g,'');
+             renderedRoute.html = renderedRoute.html.replace(/<section class="mm-category mm-category-overview">[\s\S]*?<\/section>/,'');
          } 
          renderedRoute.html = renderedRoute.html.replace(regex,'');
          renderedRoute.html = pretty(renderedRoute.html);
